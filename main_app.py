20233760b2212171040c5839c4b0d737585a2862
Shamant <143444907+Shamantak12@users.noreply.github.com>
2025-01-04 22:39:15 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -36,6 +36,10 @@ disease_solutions = {
     'Corn-Common_rust': {
         'fertilizer': 'Apply high-phosphorus fertilizers to boost the plant’s resistance.',
         'detailed_solution': 'Common rust in corn is a fungal disease that causes yellowish to orange pustules on the leaves. Use resistant varieties, apply fungicides such as tebuconazole, and remove any infected leaves to prevent the spread of the disease.'
+    },
+        'Cucumber-pinkle': {
+        'fertilizer': 'Apply high-phosphorus fertilizers to boost the plant’s resistance.',
+        'detailed_solution': 'Common rust in corn is a fungal disease that causes yellowish to orange pustules on the leaves. Use resistant varieties, apply fungicides such as tebuconazole, and remove any infected leaves to prevent the spread of the disease.'
     }
     
 
