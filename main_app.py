0c3cf96d6ae10b6d18d3a4c4976c12709df072d8
Shamant <143444907+Shamantak12@users.noreply.github.com>
2025-02-19 15:47:54 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -37,12 +37,11 @@ disease_solutions = {
         'fertilizer': 'Apply high-phosphorus fertilizers to boost the plant’s resistance.',
         'detailed_solution': 'Common rust in corn is a fungal disease that causes yellowish to orange pustules on the leaves. Use resistant varieties, apply fungicides such as tebuconazole, and remove any infected leaves to prevent the spread of the disease.'
     },
-
-     
+      'Corn-Common_rust': {
+        'fertilizer': 'Apply high-phosphorus fertilizers to boost the plant’s resistance.',
        
+    },       
         
-        
-    
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
